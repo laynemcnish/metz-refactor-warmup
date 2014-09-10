f82823b237b2e26fc3debe808d7ff44d4b2562f1
Layne McNish <lmcnish14@gmail.com>
2014-09-10 09:46:59 -0600

    separated def score into separate scoring class

--- app/models/game.rb
+++ app/models/game.rb
@@ -26,7 +26,7 @@ class Game < ActiveRecord::Base
     player = current_player
     self.available_dice -= scoring_dice.length
     self.save
-    score = score(scoring_dice)
+    score = Score.score(scoring_dice, self)
     player.current_score += score
     self.available_dice = 6 if self.available_dice == 0
     self.last_roll = roll_dice if scoring_dice == [] || score > 0
@@ -49,7 +49,7 @@ class Game < ActiveRecord::Base
     self.save
     player.total_score ||= 0
     player.current_score ||=0
-    player.current_score += score(scoring_dice)
+    player.current_score += Score.score(scoring_dice, self)
     player.total_score += player.current_score
     player.current_score = 0
     player.save
@@ -66,95 +66,5 @@ class Game < ActiveRecord::Base
     (1..self.available_dice).map { rand(1..6) }.sort.map { |face| [face, dice[face]] }
   end
 
-  def score(scoring_dice)
-    straight = scoring_dice == ['1', '2', '3', '4', '5', '6']
 
-    two_three_of_a_kind = (scoring_dice[0..2] && scoring_dice[0..2].length == 3 && scoring_dice[1..2].all? { |scoring_die| scoring_die == scoring_dice[0] }) &&
-      (scoring_dice[3..5] && scoring_dice[3..5].length == 3 && scoring_dice[4..5].all? { |scoring_die| scoring_die == scoring_dice[3] })
-
-    three_pairs = scoring_dice[0] == scoring_dice[1] &&
-      scoring_dice[2] == scoring_dice[3] &&
-      scoring_dice[4] == scoring_dice[5] &&
-      scoring_dice.length == 6 &&
-      !scoring_dice.all? { |scoring_die| scoring_dice[0] == scoring_die }
-
-    three_of_a_kind =
-      (scoring_dice[0..2] && scoring_dice[0..2].length == 3 && scoring_dice[1..2].all? { |scoring_die| scoring_die == scoring_dice[0] }) ||
-        (scoring_dice[1..3] && scoring_dice[1..3].length == 3 && scoring_dice[2..3].all? { |scoring_die| scoring_die == scoring_dice[1] }) ||
-        (scoring_dice[2..4] && scoring_dice[2..4].length == 3 && scoring_dice[3..4].all? { |scoring_die| scoring_die == scoring_dice[2] }) ||
-        (scoring_dice[3..5] && scoring_dice[3..5].length == 3 && scoring_dice[4..5].all? { |scoring_die| scoring_die == scoring_dice[3] })
-
-    four_of_a_kind =
-      (scoring_dice[0..3] && scoring_dice[0..3].length == 4 && scoring_dice[1..3].all? { |scoring_die| scoring_die == scoring_dice[0] }) ||
-        (scoring_dice[1..4] && scoring_dice[1..4].length == 4 && scoring_dice[2..4].all? { |scoring_die| scoring_die == scoring_dice[1] }) ||
-        (scoring_dice[2..5] && scoring_dice[2..5].length == 4 && scoring_dice[3..5].all? { |scoring_die| scoring_die == scoring_dice[2] })
-
-    five_of_a_kind =
-      (scoring_dice[0..4] && scoring_dice[0..4].length == 5 && scoring_dice[1..4].all? { |scoring_die| scoring_die == scoring_dice[0] }) ||
-        (scoring_dice[1..5] && scoring_dice[1..5].length == 5 && scoring_dice[2..5].all? { |scoring_die| scoring_die == scoring_dice[1] })
-
-    six_of_a_kind =
-      scoring_dice.length == 6 && scoring_dice[1..5].all? { |scoring_die| scoring_die == scoring_dice[0] }
-
-    tally_score = 0
-
-    if straight
-      tally_score = 1500
-      scoring_dice.clear
-    elsif three_pairs
-      tally_score = 750
-      scoring_dice.clear
-    elsif six_of_a_kind
-      kind = scoring_dice.find { |dice| scoring_dice.count(dice) == 6 }
-      if kind == '1'
-        tally_score = 1000 * 4
-      else
-        tally_score = kind.to_i * 100 * 2 * 2 * 2
-      end
-        scoring_dice.delete(kind)
-    elsif two_three_of_a_kind
-      kind_0 = scoring_dice[0]
-      kind_1 = scoring_dice[3]
-      if kind_0 == '1'
-        tally_score = 1000 + kind_1.to_i * 100
-      else
-        tally_score = kind_0.to_i * 100 + kind_1.to_i * 100
-      end
-      scoring_dice.clear
-    elsif five_of_a_kind
-      kind = scoring_dice.find { |dice| scoring_dice.count(dice) == 5 }
-      if kind == '1'
-        tally_score = 1000 * 3
-      else
-        tally_score = kind.to_i * 100 * 2 * 2
-      end
-        scoring_dice.delete(kind)
-    elsif four_of_a_kind
-      kind = scoring_dice.find { |dice| scoring_dice.count(dice) == 4 }
-      if kind == '1'
-        tally_score = 1000 * 2
-      else
-        tally_score = kind.to_i * 100 * 2
-      end
-        scoring_dice.delete(kind)
-    elsif three_of_a_kind
-      kind = scoring_dice.find { |dice| scoring_dice.count(dice) == 3 }
-      if kind == '1'
-        tally_score = 1000
-      else
-        tally_score = kind.first.to_i * 100
-      end
-        scoring_dice.delete(kind)
-    end
-
-    tally_score += scoring_dice.count('1') * 100 if scoring_dice.count('1') > 0 && scoring_dice.count('1') < 3
-    scoring_dice.delete('1')
-    tally_score += scoring_dice.count('5') * 50 if scoring_dice.count('5') > 0 && scoring_dice.count('5') < 3
-    scoring_dice.delete('5')
-
-    rejected_dice = scoring_dice
-
-    self.available_dice += rejected_dice.length
-    tally_score
-  end
 end
